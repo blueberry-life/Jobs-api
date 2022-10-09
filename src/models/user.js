49bb8305b898aa0dab958a87d5330dda7126617f
blueberry-life <blueberry.function@gmail.com>
2022-10-09 05:43:45 +0330

    adding user model object to project

---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please provide name"],
+    minlength: 6,
+    maxlength: 50,
+  },
+  password: {
+    type: String,
+    required: [true, "please provide password"],
+    minlength: 6,
+    maxlength: 24,
+  },
+  email: {
+    type: String,
+    required: [true, "please provide email address"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
+    unique: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
