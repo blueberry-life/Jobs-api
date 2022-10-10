7593ccc6e1dc11fc9fff5b459d7eb363267f516f
blueberry-life <blueberry.function@gmail.com>
2022-10-11 01:06:56 +0330

    adding security packages to express

--- src/express.js
+++ src/express.js
@@ -2,6 +2,13 @@ require("express-async-errors");
 
 const express = require("express");
 
+// SECTION: security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+// !SECTION
+
 // SECTION: Routes
 const authRoutes = require("./routes/auth");
 const jobsRoutes = require("./routes/jobs");
@@ -16,8 +23,19 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 const app = express();
 
-// SECTION: builtin express middleware
+// SECTION: middleware
+app.use(
+  rateLimiter({
+    // NOTE: window interval for request for per IP===15min
+    windowMs: 15 * 60 * 1000,
+    // NOTE: limits each IP to 100 requests per windowMS
+    max: 100,
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // !SECTION
 
 // SECTION: Routes
