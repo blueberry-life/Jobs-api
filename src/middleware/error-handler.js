53e06db3379a8137d1f6dedc8ab1fdae3e994758
blueberry-life <blueberry.function@gmail.com>
2022-10-10 05:35:22 +0330

    refactoring errorHandlerMiddleware

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,15 +1,30 @@
 const { StatusCodes } = require("http-status-codes");
-const CustomApiError = require("../errors/custom-error");
 
 function errorHandlerMiddleware(err, req, res, next) {
-  if (err instanceof CustomApiError) {
-    return res
-      .status(err.statusCode)
-      .json({ success: false, data: { msg: err.message } });
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong try again!!",
+  };
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = StatusCodes.BAD_REQUEST;
   }
+  if (err.name === "CastError") {
+    customError.msg = `no item find with id:(${err.value._id})please provide another id`;
+    customError.statusCode = StatusCodes.NOT_FOUND;
+  }
+  if (err.code && err.code === 11000) {
+    customError.msg = `duplicated value entered for ${Object.keys(
+      err.keyValue
+    )} field please choose another value`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
   return res
-    .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .json({ success: false, data: { msg: "some thing goes wrong !!!", err } });
+    .status(customError.statusCode)
+    .json({ success: false, data: { msg: customError.msg } });
 }
 
 module.exports = errorHandlerMiddleware;
